23bda65aecd1015e7f71ff7c632bd3d5b9a7622d
lucasrucu <lucasruiz1336@gmail.com>
2024-06-21 03:10:48 -0500

    feat: added migrations for bikes

---
+++ src/main/resources/db/migration/V005__insert_bikes.sql
@@ -0,0 +1,8 @@
+INSERT INTO bicycles (created_date, deleted, bicycle_description, bicycle_name, bicycle_price, bicycle_size, user_id)
+VALUES
+    (NOW(), 0, 'Bicycle description for Lucas 1', 'Lucas Bicycle 1', 100.0, 'Large', 'ichW0iwUKzd8exoe9BQoprJlJi93'),
+    (NOW(), 0, 'Bicycle description for Lucas 2', 'Lucas Bicycle 2', 120.0, 'Medium', 'ichW0iwUKzd8exoe9BQoprJlJi93'),
+    (NOW(), 0, 'Bicycle description for Lucas 3', 'Lucas Bicycle 3', 150.0, 'Small', 'ichW0iwUKzd8exoe9BQoprJlJi93'),
+    (NOW(), 0, 'Bicycle description for Renter 1', 'Renter Bicycle 1', 110.0, 'Large', 'JsXZvQkQidQRQ3SxpBxdwyGeE2Z2'),
+    (NOW(), 0, 'Bicycle description for Renter 2', 'Renter Bicycle 2', 130.0, 'Medium', 'JsXZvQkQidQRQ3SxpBxdwyGeE2Z2'),
+    (NOW(), 0, 'Bicycle description for Renter 3', 'Renter Bicycle 3', 160.0, 'Small', 'JsXZvQkQidQRQ3SxpBxdwyGeE2Z2');
