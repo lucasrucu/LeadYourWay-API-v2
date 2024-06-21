3a846918cc2b3f7fca13c1441dad61f6777530f7
lucasrucu <lucasruiz1336@gmail.com>
2024-06-20 23:47:56 -0500

    feat: added migration for bike

--- src/main/resources/db/migration/V001__base_line_migration.sql
+++ src/main/resources/db/migration/V001__base_line_migration.sql
@@ -22,11 +22,26 @@ CREATE TABLE cards (
     updated_date DATETIME(6),
     card_amount FLOAT NOT NULL,
     card_cvv VARCHAR(255) NOT NULL,
-    card_expiration_date DATETIME(6) NOT NULL,
+    card_expiration_date DATE NOT NULL,
     card_holder VARCHAR(255) NOT NULL,
     card_main BIT NOT NULL,
     card_number VARCHAR(255) NOT NULL,
     card_type VARCHAR(255) NOT NULL,
+    user_id VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE bicycles (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    created_date DATETIME(6) NOT NULL,
+    deleted BIT,
+    updated_date DATETIME(6),
+    bicycle_description VARCHAR(255) NOT NULL,
+    bicycle_model VARCHAR(255),
+    bicycle_name VARCHAR(255) NOT NULL,
+    bicycle_price FLOAT(53) NOT NULL,
+    bicycle_size VARCHAR(255) NOT NULL,
+    image_data VARCHAR(255),
     user_id VARCHAR(255),
     PRIMARY KEY (id)
 );
@@ -36,3 +51,6 @@ ALTER TABLE users
 
 ALTER TABLE cards
     ADD CONSTRAINT fk_cards_users FOREIGN KEY (user_id) REFERENCES users(id);
+
+ALTER TABLE bicycles
+    ADD CONSTRAINT fk_bicycles_users FOREIGN KEY (user_id) REFERENCES users(id);
