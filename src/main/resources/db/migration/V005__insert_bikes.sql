913ccf87cb7571ed0523baadd8324f7346796d72
lucasrucu <lucasruiz1336@gmail.com>
2024-06-21 14:33:06 -0500

    update: updated biek migrations

--- src/main/resources/db/migration/V005__insert_bikes.sql
+++ src/main/resources/db/migration/V005__insert_bikes.sql
@@ -1,8 +1,8 @@
-INSERT INTO bicycles (created_date, deleted, bicycle_description, bicycle_name, bicycle_price, bicycle_size, user_id)
+INSERT INTO bicycles (created_date, deleted, bicycle_description, bicycle_name, bicycle_price, bicycle_size, user_id, image_data)
 VALUES
-    (NOW(), 0, 'Bicycle description for Lucas 1', 'Lucas Bicycle 1', 100.0, 'Large', 'ichW0iwUKzd8exoe9BQoprJlJi93'),
-    (NOW(), 0, 'Bicycle description for Lucas 2', 'Lucas Bicycle 2', 120.0, 'Medium', 'ichW0iwUKzd8exoe9BQoprJlJi93'),
-    (NOW(), 0, 'Bicycle description for Lucas 3', 'Lucas Bicycle 3', 150.0, 'Small', 'ichW0iwUKzd8exoe9BQoprJlJi93'),
-    (NOW(), 0, 'Bicycle description for Renter 1', 'Renter Bicycle 1', 110.0, 'Large', 'JsXZvQkQidQRQ3SxpBxdwyGeE2Z2'),
-    (NOW(), 0, 'Bicycle description for Renter 2', 'Renter Bicycle 2', 130.0, 'Medium', 'JsXZvQkQidQRQ3SxpBxdwyGeE2Z2'),
-    (NOW(), 0, 'Bicycle description for Renter 3', 'Renter Bicycle 3', 160.0, 'Small', 'JsXZvQkQidQRQ3SxpBxdwyGeE2Z2');
+    (NOW(), 0, 'Bicycle description for Lucas 1', 'Lucas Bicycle 1', 100.0, 'Large', 'ichW0iwUKzd8exoe9BQoprJlJi93', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGbaV7wgHH6vfMiUgiGVyMQvUUN_unu0Oc4g&s'),
+    (NOW(), 0, 'Bicycle description for Lucas 2', 'Lucas Bicycle 2', 120.0, 'Medium', 'ichW0iwUKzd8exoe9BQoprJlJi93', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv0ITM8BewtWESPOtocg-hFt-sdXtol0UCKQ&s'),
+    (NOW(), 0, 'Bicycle description for Lucas 3', 'Lucas Bicycle 3', 150.0, 'Small', 'ichW0iwUKzd8exoe9BQoprJlJi93', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRunXVvymb9MdGcnDBojTsPccA5z4co9l5ubQ&s'),
+    (NOW(), 0, 'Bicycle description for Renter 1', 'Renter Bicycle 1', 110.0, 'Large', 'JsXZvQkQidQRQ3SxpBxdwyGeE2Z2', 'https://www.cnet.com/a/img/resize/d03415b3ec84abd6b5a1d049f85ef4d187d392b8/hub/2020/09/17/abed09d8-4910-4ab6-8fba-601e22431573/20200830-trek-fuel-ex-8-mountain-bike-01.jpg?auto=webp&fit=crop&height=1200&precrop=6573,4233,x0,y0&width=1200'),
+    (NOW(), 0, 'Bicycle description for Renter 2', 'Renter Bicycle 2', 130.0, 'Medium', 'JsXZvQkQidQRQ3SxpBxdwyGeE2Z2', 'https://m.media-amazon.com/images/I/81DPZ3XfrwL._AC_UF894,1000_QL80_.jpg'),
+    (NOW(), 0, 'Bicycle description for Renter 3', 'Renter Bicycle 3', 160.0, 'Small', 'JsXZvQkQidQRQ3SxpBxdwyGeE2Z2', 'https://images.bikesonline.com/assets/thumbL/AITPX26TRD13G1.jpg?tr=w-334,h-220');
