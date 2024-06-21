7a22d14e648545dcd6edf46db3b1ffd9f5cc8179
lucasrucu <lucasruiz1336@gmail.com>
2024-06-20 23:24:27 -0500

    feat: finished application layer for bikes

---
+++ src/main/java/com/lyw/api/app/core/bicycle/application/implementation/BicycleCommanServiceImpl.java
@@ -0,0 +1,55 @@
+package com.lyw.api.app.core.bicycle.application.implementation;
+
+import org.springframework.stereotype.Service;
+
+import com.lyw.api.app.core.bicycle.application.services.BicycleCommandService;
+import com.lyw.api.app.core.bicycle.domain.commands.CreateBicycleCommand;
+import com.lyw.api.app.core.bicycle.domain.commands.DeleteBicycleCommand;
+import com.lyw.api.app.core.bicycle.domain.commands.UpdateBicycleCommand;
+import com.lyw.api.app.core.bicycle.domain.model.Bicycle;
+import com.lyw.api.app.core.bicycle.domain.repositories.BicycleRepository;
+import com.lyw.api.app.core.bicycle.infrastructure.dto.BicycleRequestDto;
+import com.lyw.api.app.shared.utils.ValidationUtil;
+
+@Service
+public class BicycleCommanServiceImpl implements BicycleCommandService {
+
+    private final BicycleRepository bicycleRepository;
+    private final ValidationUtil validationUtil;
+
+    public BicycleCommanServiceImpl(BicycleRepository bicycleRepository, ValidationUtil validationUtil) {
+        this.bicycleRepository = bicycleRepository;
+        this.validationUtil = validationUtil;
+    }
+
+    @Override
+    public Bicycle handle(CreateBicycleCommand command) {
+        Bicycle bicycle = new Bicycle();
+        bicycle = setBicycle(bicycle, command.bicycleRequestDto());
+        bicycle.setUser(validationUtil.findUserById(command.userId()));
+        return bicycleRepository.save(bicycle);
+    }
+
+    @Override
+    public Bicycle handle(UpdateBicycleCommand command) {
+        Bicycle bicycle = validationUtil.findBicycleById(command.bicycleId());
+        bicycle = setBicycle(bicycle, command.bicycleRequestDto());
+        return bicycleRepository.save(bicycle);
+    }
+
+    @Override
+    public void handle(DeleteBicycleCommand command) {
+        Bicycle bicycle = validationUtil.findBicycleById(command.bicycleId());
+        bicycleRepository.delete(bicycle);
+    }
+
+    private Bicycle setBicycle(Bicycle bicycle, BicycleRequestDto bicycleRequestDto) {
+        bicycle.setBicycleName(bicycleRequestDto.getBicycleName());
+        bicycle.setBicycleDescription(bicycleRequestDto.getBicycleDescription());
+        bicycle.setBicyclePrice(bicycleRequestDto.getBicyclePrice());
+        bicycle.setBicycleSize(bicycleRequestDto.getBicycleSize());
+        bicycle.setBicycleModel(bicycleRequestDto.getBicycleModel());
+        bicycle.setImageData(bicycleRequestDto.getImageData());
+        return bicycle;
+    }
+}
---
+++ src/main/java/com/lyw/api/app/core/bicycle/application/services/BicycleQueryService.java
@@ -0,0 +1,16 @@
+package com.lyw.api.app.core.bicycle.application.services;
+
+import java.util.List;
+
+import com.lyw.api.app.core.bicycle.domain.model.Bicycle;
+import com.lyw.api.app.core.bicycle.domain.queries.GetAvailableBicyclesQuery;
+import com.lyw.api.app.core.bicycle.domain.queries.GetBicycleByIdQuery;
+import com.lyw.api.app.core.bicycle.domain.queries.GetBicyclesQuery;
+
+public interface BicycleQueryService {
+    Bicycle handle(GetBicycleByIdQuery query);
+
+    List<Bicycle> handle(GetBicyclesQuery query);
+
+    List<Bicycle> handle(GetAvailableBicyclesQuery query);
+}
