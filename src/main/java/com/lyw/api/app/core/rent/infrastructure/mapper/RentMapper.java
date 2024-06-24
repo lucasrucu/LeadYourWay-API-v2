5607df794e81ac8dcc14826f4cd38f3a6e7368ec
StevensAcha <u20201e735@upc.edu.pe>
2024-06-23 19:43:46 -0500

    fix: RentMapper convertion to long and service annotation

--- src/main/java/com/lyw/api/app/core/rent/infrastructure/mapper/RentMapper.java
+++ src/main/java/com/lyw/api/app/core/rent/infrastructure/mapper/RentMapper.java
@@ -3,7 +3,10 @@ package com.lyw.api.app.core.rent.infrastructure.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+import org.mapstruct.Named;
 
+import com.lyw.api.app.core.bicycle.domain.model.Bicycle;
+import com.lyw.api.app.core.card.domain.model.Card;
 import com.lyw.api.app.core.rent.domain.model.Rent;
 import com.lyw.api.app.core.rent.infrastructure.dto.RentResponseDto;
 
@@ -15,10 +18,20 @@ public interface RentMapper {
             @Mapping(target = "rentStartDate", source = "rentStartDate"),
             @Mapping(target = "rentEndDate", source = "rentEndDate"),
             @Mapping(target = "rentPrice", source = "rentPrice"),
-            @Mapping(target = "bicycleId", source = "bicycleId"),
-            @Mapping(target = "cardId", source = "cardId")
+            @Mapping(target = "bicycleId", source = "bicycle", qualifiedByName = "mapBicycle"),
+            @Mapping(target = "cardId", source = "card", qualifiedByName = "mapCard")
     })
     RentResponseDto toResponseDto(Rent rent);
 
     List<RentResponseDto> toResponseDto(List<Rent> rents);
+
+    @Named("mapBicycle")
+    default Long mapBicycle(Bicycle bicycle) {
+        return bicycle.getId();
+    }
+
+    @Named("mapCard")
+    default Long mapCard(Card card) {
+        return card.getId();
+    }
 }
