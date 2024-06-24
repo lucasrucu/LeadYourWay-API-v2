fce770443ccc3cae01a0684789166b812cde305f
lucasrucu <lucasruiz1336@gmail.com>
2024-06-24 00:31:46 -0500

    feat: endpoint for user patch

--- src/main/java/com/lyw/api/app/iam/identity/application/implementation/UserCommandServiceImpl.java
+++ src/main/java/com/lyw/api/app/iam/identity/application/implementation/UserCommandServiceImpl.java
@@ -3,6 +3,7 @@ package com.lyw.api.app.iam.identity.application.implementation;
 import org.springframework.stereotype.Service;
 
 import com.lyw.api.app.iam.identity.application.services.UserCommandService;
+import com.lyw.api.app.iam.identity.domain.commands.PatchUserCommand;
 import com.lyw.api.app.iam.identity.domain.commands.RegisterUserBikerCommand;
 import com.lyw.api.app.iam.identity.domain.commands.RegisterUserRenterCommand;
 import com.lyw.api.app.iam.identity.domain.model.Role;
@@ -34,6 +35,13 @@ public class UserCommandServiceImpl implements UserCommandService {
         User user = new User();
         Role role = validationUtil.getRenterRole();
         return getUser(user, role, registerUserCommand.userRequestDto());
+    }
+
+    @Override
+    public User handle(PatchUserCommand patchUserCommand) {
+        User user = validationUtil.findUserById(patchUserCommand.userRequestDto().getId());
+        Role role = user.getRole();
+        return getUser(user, role, patchUserCommand.userRequestDto());
     }
 
     private User getUser(User user, Role role, UserRequestDto userRequestDto) {
